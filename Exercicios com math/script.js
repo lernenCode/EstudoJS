179ef52344636e22ccdcd579db22b0d40cd58d50
LERN-DEV <luizvinicius.emprego@hotmail.com>
2022-12-26 16:06:17 -0300

    math

---
+++ Exercicios com math/script.js
@@ -0,0 +1,17 @@
+const num = parseFloat(prompt('digite um numero'));
+
+const Numero = document.getElementById('numero'); 
+Numero.innerHTML = num;
+
+const Raiz = document.getElementById('raiz');
+Raiz.innerHTML = Math.sqrt(num);
+
+const Inteiro = document.getElementById('inteiro');
+Inteiro.innerHTML = Number.isInteger(num);
+
+const Arredondado = document.getElementById('arredondado'); 
+Arredondado.innerHTML = Math.ceil(num);
+
+const Decimal = document.getElementById('decimal'); 
+Decimal.innerHTML = num.toFixed(2);
+

8da56ef1020873f134adcf96a030562a94945098
LERN-DEV <luizvinicius.emprego@hotmail.com>
2023-01-11 12:21:51 -0300

    exercicios

--- exercicios alert promt confirm/script.js
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-let num1 = prompt('Digite um numero');
-let num2 = prompt('Digite um numero');
-=======
-let num1 = prompt('Digite um numero');
-let num2 = prompt('Digite um numero');
->>>>>>> c5c77f34be9adfb83a0e9ba6d4e57d3eee9e6c40
-alert(`O resultado é ${Number(num1) + Number(num2)}`);

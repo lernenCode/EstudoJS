5150828828708e8c39afc2a48ee04114b8eb3df4
LERN-DEV <luizvinicius.emprego@hotmail.com>
2023-01-11 12:20:08 -0300

    exercicios

---
+++ exercicio com let/variavel.js
@@ -0,0 +1,5 @@
+
+let nome = 'joão'; 
+
+console.log(nome,'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
---
+++ exercicios com alert promt confirm/script.js
@@ -0,0 +1,5 @@
+
+let num1 = prompt('Digite um numero');
+let num2 = prompt('Digite um numero');
+
+alert(`O resultado é ${Number(num1) + Number(num2)}`);

f70a631851ee87a6bd98b11cd6b178d678848e01
LERN-DEV <luizvinicius.emprego@hotmail.com>
2023-01-25 17:25:36 -0300

    exercicio com date  toLocaleDateString

--- exercicio com date/main.js
+++ exercicio com date/main.js
@@ -1,6 +1,7 @@
 const data = document.querySelector('#data');
 const date = new Date();
 
+/*
 const diasDaSemana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira"];
 const mesesDoAno = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
@@ -9,6 +10,8 @@ const anoTexto = mesesDoAno[date.getMonth()];
 
 const hora = (date.getHours() <10 ? '0' : '') + date.getHours();
 const minutos = (date.getMinutes() <10 ? '0' : '') + date.getMinutes();
-
 data.innerHTML = `${semanaTexto}, ${date.getDate()} de ${anoTexto} de ${date.getFullYear()} 
-${hora}:${minutos}`; 
+${hora}:${minutos}`;
+*/ 
+
+data.innerHTML =  `${date.toLocaleDateString('pt-BR', {dateStyle:"full"})} ${date.toLocaleTimeString('pt-BR', {hour: '2-digit', minute:'2-digit'})}`;
